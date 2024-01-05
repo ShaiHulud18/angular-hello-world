1a6282987ff78a0258d5dea0ec4a817efb93e8bb
shailhulud18 <ashish.aravinda@gmail.com>
2024-01-05 15:01:02 -0600

    Created a basic angular app called hello world... Just a start, need to push more....

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-
+import { HelloWorldComponent } from './hello-world/hello-world.component';
+import { UserItemComponent } from './user-item/user-item.component'
+import { UserListComponent } from './user-list/user-list.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,HelloWorldComponent,UserItemComponent,UserListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
